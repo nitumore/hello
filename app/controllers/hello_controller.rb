7076a9cb7984719f065f3cd3895fc36e2f43da71
Nitin More <nmore2012@gmail.com>
2012-10-31 18:02:50 +0530

    Initial commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,7 @@
+class HelloController < ApplicationController
+
+	def welcome
+                @message = 'Hello World! My first Ruby on Rails 3 program'
+    end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '7db8cc01801279bbf7016437f66c21823e93c3a21c482356c7a964e6a12e74e5a8249f19d0723cc408ce387cdd594b1036b32a8c08ed4dc3cc1648b47f09673d'
